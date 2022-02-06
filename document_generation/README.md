7fea861904b92c39a6f5b0e67336fe9ef9617888
Yixuan Su <syxistheone@gmail.com>
2022-02-06 11:12:31 +0000

    Add files via upload

---
+++ document_generation/README.md
@@ -0,0 +1,191 @@
+## This repo describes the experimental details on Wikitext-103 benchmark.
+****
+### Catalogue:
+* <a href='#data_preparation'>1. Data Preparation</a>
+* <a href='#train_simctg'>2. Train SimCTG</a>
+* <a href='#generate_results'>3. Generate Result with Different Decoding Methods</a>
+    * <a href='#contrastive_search'>3.1. Contrastive Search</a>
+    * <a href='#diverse_contrastive_search'>3.2. Diverse Contrastive Search</a>
+    * <a href='#nucleus_sampling'>3.3. Nucleus Sampling</a>
+    * <a href='#greedy_search'>3.4. Greedy Search</a>
+    * <a href='#beam_search'>3.5. Beam Search</a>
+* <a href='#visualize_token_similarity_matrix'>4. Visualize Token Similarity Matrix</a>
+
+
+****
+<span id='data_preparation'/>
+
+#### 1. Data Preparation:
+To download the data, please follow the instructions [here](https://github.com/yxuansu/SimCTG/tree/main/data).
+
+> **** The dataset contains the following three files:
+
+    .
+    ├── wikitext103                       
+        ├── wikitext103_raw_v1_train.txt          # Training Set
+        ├── wikitext103_raw_v1_validation.txt     # Validation Set
+        └── wikitext103_raw_v1_test.txt           # Test Set
+
+**Data Format**: In the files, each line represents a full document.
+
+****
+
+<span id='train_simctg'/>
+
+#### 2. Train SimCTG:
+To train a SimCTG model on Wikitext-103, please run the following commands:
+```yaml
+chmod +x ./train.sh
+./train.sh
+```
+The arguments are as follows:
+* `--model_name`: The name of huggingface pre-trained gpt model (e.g. gpt2, gpt-large).
+* `--train_path`: The file path of training set.
+* `--dev_path`: The file path of validation set.
+* `--test_path`: The file path of test set.
+* `--margin`: The contrastive margin $\rho$.
+* `--max_len`: The maximum length of training samples.
+* `--number_of_gpu`: The number of available GPUs.
+* `--batch_size_per_gpu`: The batch size for each GPU.
+* `--gradient_accumulation_steps`: How many forward computations between two gradient updates.
+* `--effective_batch_size`: The overall batch size. It equals to batch_size_per_gpu x gradient_accumulation_steps x number_of_gpu.
+* `--total_steps`: The number of total gradient update steps.
+* `--print_every`: Have many steps to show the intermediate results.
+* `--save_every`: How many steps to save one checkpoint.
+* `--learning_rate`: The learning rate.
+* `--save_path_prefix`: Where to save the checkpoints.
+
+****
+<span id='generate_results'/>
+
+#### 3. Generate Result with Different Decoding Methods:
+Here, we use the prefix in Table 3 of the [paper]() to illustrate how to use different decoding methods to generate the result. 
+```python
+import torch
+from simctg import SimCTG
+from transformers import AutoTokenizer
+# load model and tokenizer
+model_path = r'cambridgeltl/simctg_wikitext103'
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = SimCTG(model_path, tokenizer.pad_token_id)
+model.eval()
+
+# prepare prefix input
+text = r"Butt criticized Donald 's controls in certain situations in the game , as well as the difficulty of some levels and puzzles . Buchanan also criticized the controls , calling"
+tokens = tokenizer.tokenize(text)
+input_ids = tokenizer.convert_tokens_to_ids(tokens)
+input_ids = torch.LongTensor(input_ids).view(1,-1)
+```
+<span id='contrastive_search'/>
+
+##### 3.1. Contrastive Search:
+```python
+# use contrastive search to generate the result
+beam_width, alpha, decoding_len = 8, 0.6, 128
+output = model.fast_contrastive_search(input_ids, beam_width, alpha, decoding_len)
+#output = model.slow_contrastive_search(input_ids, beam_width, alpha, decoding_len)
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(output))
+```
+The arguments are as follows:
+* `--input_ids`: The ids of the prefix sequence.
+* `--beam_width`: k in the contrastive search, which is typically set within the range of [5,10].
+* `--alpha`: alpha in the contrastive search, which is typically set within the range of [0.5,0.8].
+* `--decoding_len`: Number of tokens to generate.
+
+**[Note]** We provide two implementations of contrastive search: (1) fast_contrastive_search and (2) slow_contrastive_search. These two implementations produce the same result, but the fast version is properly optimized and is much faster than the slow version. On the other hand, the implementation details of the slow version is more straightforward. We recommend you to rewrite the slow version first if you want to adapt contrastive search to your specific research task.
+
+<span id='diverse_contrastive_search'/>
+
+##### 3.2. Diverse Contrastive Search:
+We also provide a stochastic version of contrastive search which can generate diverse results by combining nucleus sampling and contrastive search. More details can be found in Appendix E of the [paper]().
+```python
+# use diverse contrastive search to generate the result
+sample_step, nucleus_p = 2, 0.95
+beam_width, alpha, decoding_len = 8, 0.6, 128
+output = model.diverse_contrastive_search(input_ids, sample_step, nucleus_p, beam_width, alpha, decoding_len)
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(output))
+```
+The arguments are as follows:
+* `--input_ids`: The ids of the prefix sequence.
+* `--sample_step`: The number of tokens sampled with nucleus sampling at the start of generation process.
+* `--nucleus_p`: The probability in nucleus sampling.
+* `--beam_width`: k in the contrastive search, which is typically set within the range of [5,10].
+* `--alpha`: alpha in the contrastive search, which is typically set within the range of [0.5,0.8].
+* `--decoding_len`: The total number of tokens to generate. It equals to the number of sampled tokens + the number of tokens generated by contrastive search.
+
+<span id='nucleus_sampling'/>
+
+##### 3.3. Nucleus Sampling:
+```python
+nucleus_p, decoding_len = 0.95, 128
+output = model.nucleus_sampling(input_ids, nucleus_p, decoding_len)
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(output))
+```
+The arguments are as follows:
+* `--input_ids`: The ids of the prefix sequence.
+* `--nucleus_p`: The probability in nucleus sampling.
+* `--decoding_len`: Number of tokens to generate.
+
+<span id='greedy_search'/>
+
+##### 3.4. Greedy Search:
+```python
+decoding_len = 128
+output = model.greedy_search(input_ids, decoding_len)
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(output))
+```
+The arguments are as follows:
+* `--input_ids`: The ids of the prefix sequence.
+* `--decoding_len`: Number of tokens to generate.
+
+<span id='beam_search'/>
+
+##### 3.5. Beam Search:
+```python
+beam_width, decoding_len = 10, 128
+output = model.beam_search(input_ids, beam_width, decoding_len)
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(output))
+```
+The arguments are as follows:
+* `--input_ids`: The ids of the prefix sequence.
+* `--beam_width`: The beam width of beam search.
+* `--decoding_len`: Number of tokens to generate.
+
+****
+
+<span id='visualize_token_similarity_matrix'/>
+
+#### 4. Visualize Token Similarity Matrix
+Here, we show how to reproduce the token similarity matrix visualization (Figure 6 of the paper).
+```python
+import torch
+from simctg import SimCTG
+from transformers import AutoTokenizer
+# load model and tokenizer
+model_path = r'cambridgeltl/simctg_wikitext103'
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = SimCTG(model_path, tokenizer.pad_token_id)
+model.eval()
+
+# prepare prefix input
+text = r"Butt criticized Donald 's controls in certain situations in the game , as well as the difficulty of some levels and puzzles . Buchanan also criticized the controls , calling"
+tokens = tokenizer.tokenize(text)
+input_ids = tokenizer.convert_tokens_to_ids(tokens)
+input_ids = torch.LongTensor(input_ids).view(1,-1)
+
+# use contrastive search to generate the result
+beam_width, alpha, decoding_len = 8, 0.6, 128
+output = model.fast_contrastive_search(input_ids, beam_width, alpha, decoding_len)
+
+# save the visualization result
+model.save_token_similarity_map(output, save_name='token_similarity_matrix_visualization.png')
+```
+The arguments are as follows:
+* `--output`: The ids of decoded result.
+* `--save_name`: The saved name of the visualization result.
+
---
+++ document_generation/simctg.py
@@ -0,0 +1,207 @@
+import sys
+import os
+import operator
+from operator import itemgetter
+import torch
+from torch import nn
+import random
+import argparse
+import numpy as np
+import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+from loss_func import contrastive_loss
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+train_fct = CrossEntropyLoss()
+val_fct = CrossEntropyLoss(reduction='none')
+class SimCTG(nn.Module):
+    def __init__(self, model_name, pad_token_id):
+        super(SimCTG, self).__init__()
+        from transformers import AutoTokenizer, GPT2LMHeadModel
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.vocab_size = len(self.tokenizer)
+        self.model = GPT2LMHeadModel.from_pretrained(model_name)
+        self.vocab_size = len(self.tokenizer)
+        self.embed_dim = self.model.config.hidden_size
+        self.pad_token_id = pad_token_id
+
+    def compute_logits_and_hidden_states(self, input_ids):
+        # used for advanced decoding
+        # input_ids: 1 x seqlen
+        outputs = self.model(input_ids=input_ids, output_hidden_states=True)
+        last_hidden_states = outputs.hidden_states[-1]
+        logits = outputs.logits
+        return last_hidden_states, logits
+
+    def forward(self, input_ids, labels, margin):
+        bsz, seqlen = input_ids.size()
+        outputs = self.model(input_ids=input_ids, output_hidden_states=True)
+        logits = outputs.logits
+        assert logits.size() == torch.Size([bsz, seqlen, self.vocab_size])
+        last_hidden_states = outputs.hidden_states[-1]
+        assert last_hidden_states.size() == torch.Size([bsz, seqlen, self.embed_dim])
+        mle_loss = train_fct(logits.view(-1, self.vocab_size), labels.view(-1))
+
+        norm_rep = last_hidden_states / last_hidden_states.norm(dim=2, keepdim=True)
+        cosine_scores = torch.matmul(norm_rep, norm_rep.transpose(1,2)) 
+        assert cosine_scores.size() == torch.Size([bsz, seqlen, seqlen])
+        cl_loss = contrastive_loss(margin, cosine_scores, input_ids, self.pad_token_id, prefix_len=0)
+        return mle_loss, cl_loss
+
+    def eval_loss(self, input_ids, labels):
+        bsz, seqlen = input_ids.size()
+        outputs = self.model(input_ids=input_ids, output_hidden_states=True)
+        logits = outputs.logits
+        assert logits.size() == torch.Size([bsz, seqlen, self.vocab_size])
+        last_hidden_states = outputs.hidden_states[-1]
+        assert last_hidden_states.size() == torch.Size([bsz, seqlen, self.embed_dim])
+        mle_loss = val_fct(logits.view(-1, self.vocab_size), labels.view(-1))
+        assert mle_loss.size() == torch.Size([bsz * seqlen])
+        mask_tmp = labels.masked_fill(~labels.eq(-100), 1.0)
+        mask = mask_tmp.masked_fill(mask_tmp.eq(-100), 0.0)
+        # sum 
+        mle_loss_sum = torch.sum(mle_loss)
+        token_num_sum = torch.sum(mask)
+        return mle_loss_sum, token_num_sum
+
+    def save_model(self, ckpt_save_path):
+        import os
+        if os.path.exists(ckpt_save_path):
+            pass
+        else: # recursively construct directory
+            os.makedirs(ckpt_save_path, exist_ok=True)
+        # save model
+        self.model.save_pretrained(ckpt_save_path)
+        # save tokenizer
+        self.tokenizer.save_pretrained(ckpt_save_path)
+
+    # decoding functions
+    # ------------------------------------------------------- #
+    def slow_contrastive_search(self, input_ids, beam_width, alpha, decoding_len):
+        '''
+           input_ids: prefix input; 1 x prefix_len
+           decoding_len: how many tokens to generate
+           beam_width: size of candidate pool during decoding
+           alpha: regulates importance of model confidence and degeneration penalty
+        '''
+        # sanity check
+        # sanity check
+        assert alpha >= 0. and alpha <= 1.0
+
+        from utlis import ContrastiveDecodingOneStep
+        for step in range(decoding_len):
+            input_ids = ContrastiveDecodingOneStep(self, input_ids, beam_width, alpha)
+        return input_ids[0]
+
+    def fast_contrastive_search(self, input_ids, beam_width, alpha, decoding_len):
+        '''
+           input_ids: prefix input; 1 x prefix_len
+           decoding_len: how many tokens to generate
+           beam_width: size of candidate pool during decoding
+           alpha: regulates importance of model confidence and degeneration penalty
+        '''
+        self.model.eval()
+        from utlis import ContrastiveDecodingOneStepFast
+        # sanity check
+        assert alpha >= 0. and alpha <= 1.0
+        
+        # fast mode
+        batch_size, seqlen = input_ids.size()
+        #generated = [[] for _ in range(batch_size)]
+        generated = [item for item in input_ids.tolist()]
+        past_key_values = None
+        last_hidden_states = None
+        logits = None
+        for step in range(decoding_len):
+            input_ids, past_key_values, last_hidden_states, logits = ContrastiveDecodingOneStepFast(
+                self.model,
+                input_ids,
+                beam_width,
+                alpha,
+                past_key_values,
+                last_hidden_states,
+                self.tokenizer,
+                logits,
+                first_step=step == 0,
+            )
+            tokens = input_ids.squeeze(dim=-1).tolist()
+            for idx, t in enumerate(tokens):
+                generated[idx].append(t)
+        return generated[0]
+
+    def diverse_contrastive_search(self, input_ids, sample_step, nucleus_p, beam_width, alpha, decoding_len):
+        '''
+            sample_step: 
+                number of steps to decode with nucleus sampling, 
+                for the remaining steps we use contrastive search
+            decoding_len: 
+                the total number of generated tokens
+            beam_width: 
+                size of candidate pool during decoding
+            alpha: 
+                regulates importance of model confidence and degeneration penalty
+
+        '''
+        contrastive_step = decoding_len - sample_step
+        _, prefix_len = input_ids.size()
+        # first do sample
+        input_ids = self.model.generate(
+                            input_ids, 
+                            do_sample=True, 
+                            max_length=prefix_len+sample_step, 
+                            top_p=nucleus_p,
+                            top_k=0)
+        # then do contrastive search
+        output = self.fast_contrastive_search(input_ids, beam_width, alpha, contrastive_step)
+        return output
+
+    def greedy_search(self, input_ids, decoding_len):
+        _, prefix_len = input_ids.size()
+        output = self.model.generate(
+                            input_ids, 
+                            max_length=prefix_len+decoding_len)
+        return output[0]
+
+    def beam_search(self, input_ids, beam_width, decoding_len):
+        _, prefix_len = input_ids.size()
+        output = self.model.generate(
+                            input_ids, 
+                            max_length=prefix_len+decoding_len, 
+                            num_beams=beam_width)
+        return output[0]
+
+
+    def nucleus_sampling(self, input_ids, nucleus_p, decoding_len):
+        _, prefix_len = input_ids.size()
+        output = self.model.generate(
+                            input_ids, 
+                            do_sample=True, 
+                            max_length=prefix_len+decoding_len, 
+                            top_p=nucleus_p,
+                            top_k=0)
+        return output[0]
+    # ------------------------------------------------------- #
+
+    def compute_correlation_matrix(self, input_ids):        
+        _, seq_len = input_ids.size()
+        hidden = self.model.base_model(input_ids).last_hidden_state
+        #print (hidden)
+        norm_hidden = hidden / hidden.norm(dim=2, keepdim=True)
+        correlation_matrix = torch.matmul(norm_hidden, norm_hidden.transpose(1,2)).view(seq_len, seq_len)
+        return correlation_matrix.detach().numpy()
+
+    # to produce similarity matrix heatmap
+    def save_token_similarity_map(self, input_ids, save_name):
+        input_ids = torch.LongTensor(input_ids).view(1, -1)
+        correlation_matrix = self.compute_correlation_matrix(input_ids)
+        df = pd.DataFrame(correlation_matrix)
+        df.to_string(index=False)
+        df.style.hide_index()
+        df.style.hide_index()
+        sns.heatmap(df, cmap="Blues", xticklabels=False, yticklabels=False)
+        plt.savefig(save_name, format='png', dpi=500, bbox_inches = 'tight')
+        plt.show()
+
