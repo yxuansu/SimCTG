0f109783534e775991de6a5949969bdd603e17a4
Yixuan Su <syxistheone@gmail.com>
2022-04-03 10:23:11 +0100

    Update train_rocstories.sh

--- story_generation/train_rocstories.sh
+++ story_generation/train_rocstories.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=0,1 python train.py\
     --batch_size_per_gpu 16\
     --gradient_accumulation_steps 8\
     --effective_batch_size 256\
-    --total_steps 20000\
+    --total_steps 8000\
     --print_every 100\
     --save_every 500\
     --learning_rate 2e-5\
