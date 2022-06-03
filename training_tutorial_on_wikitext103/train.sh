3c9be3d2280849bc8903e63924c47e4ae070e6f8
Yixuan Su <syxistheone@gmail.com>
2022-06-03 20:56:16 +0100

    Update train.sh

--- training_tutorial_on_wikitext103/train.sh
+++ training_tutorial_on_wikitext103/train.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=3 python train.py\
+CUDA_VISIBLE_DEVICES=0 python train.py\
     --model_name gpt2\
     --train_path ../data/language_modelling/wikitext103/wikitext103_raw_v1_train.txt\
     --dev_path ../data/language_modelling/wikitext103/wikitext103_raw_v1_validation.txt\
